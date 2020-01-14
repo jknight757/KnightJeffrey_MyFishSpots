bb0425136c86c5b20181f241ab3eae766baf2d42
Jeffrey Knight <Jeffrey.757Knight@gmail.com>
2020-01-14 13:58:40 -0500

    Commit 2
    In this commit, Google maps android SDK is implemented. The main map
    feature is in its first stages of development.

--- app/src/main/java/com/example/knightjeffrey_myfishspots/MainActivity.java
+++ app/src/main/java/com/example/knightjeffrey_myfishspots/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.knightjeffrey_myfishspots.fragments.MainMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.map_fragment_container, MainMapFragment.newInstance(),null).commit();
     }
 }
--- app/src/main/java/com/example/knightjeffrey_myfishspots/fragments/MainMapFragment.java
+++ app/src/main/java/com/example/knightjeffrey_myfishspots/fragments/MainMapFragment.java
@@ -8,25 +8,93 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.example.knightjeffrey_myfishspots.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainMapFragment extends Fragment {
+public class MainMapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
 
+    public static final String TAG = "MapFragment.TAG";
+    private final double mOfficialLat = 28.590647;
+    private final double mOfficialLong = -81.304510;
 
-    public MainMapFragment() {
-        // Required empty public constructor
+    private GoogleMap mMap;
+
+    public static MainMapFragment newInstance() {
+
+        Bundle args = new Bundle();
+        MainMapFragment fragment = new MainMapFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_map, container, false);
+    public void onActivityCreated(Bundle bundle) {
+        super.onActivityCreated(bundle);
+
+        getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setInfoWindowAdapter(this);
+        mMap.setOnInfoWindowClickListener(this);
+
+        zoomInCamera();
+        addMapMarker();
+    }
+    private void addMapMarker(){
+        if(mMap == null){
+            return;
+        }
+
+        MarkerOptions options = new MarkerOptions();
+        options.title("Fullsail university");
+        options.snippet("Building 4");
+
+        LatLng officalLocation = new LatLng(mOfficialLat,mOfficialLong);
+        options.position(officalLocation);
+
+        mMap.addMarker(options);
+    }
+    private void zoomInCamera(){
+        if(mMap == null){
+            return;
+        }
+
+        LatLng officalLocation = new LatLng(mOfficialLat,mOfficialLong);
+        CameraUpdate cameraMovement = CameraUpdateFactory.newLatLngZoom(officalLocation, 16);
+        mMap.animateCamera(cameraMovement);
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) { return null;}
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        View contents = LayoutInflater.from(getActivity()).inflate(R.layout.info_window, null);
+
+        ((TextView)contents.findViewById(R.id.title)).setText(marker.getTitle());
+        ((TextView)contents.findViewById(R.id.snippet)).setText(marker.getSnippet());
+        return contents;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
 }
