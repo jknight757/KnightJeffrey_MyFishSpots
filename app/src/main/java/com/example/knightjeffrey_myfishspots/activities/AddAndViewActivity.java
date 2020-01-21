3a718acc8d38b4e1811f24e2aeb7d6cb0d8f2b70
Jeffrey Knight <Jeffrey.757Knight@gmail.com>
2020-01-21 18:37:58 -0500

    Commit 6
    This commit provides additional functionality for adding a new marker.
    The user can search coordinates and the location will be displayed on
    the map with a temporary marker. They can also press and hold a
    location on the map to add a temporary marker to be added.

--- app/src/main/java/com/example/knightjeffrey_myfishspots/activities/AddAndViewActivity.java
+++ app/src/main/java/com/example/knightjeffrey_myfishspots/activities/AddAndViewActivity.java
@@ -3,16 +3,19 @@ package com.example.knightjeffrey_myfishspots.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.knightjeffrey_myfishspots.R;
 import com.example.knightjeffrey_myfishspots.fragments.MainMapFragment;
 
-public class AddAndViewActivity extends AppCompatActivity {
+public class AddAndViewActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText latInput;
     EditText longInput;
-    private static final int ADD_NEW_STATE = 0006;
+
+    private static final int ADD_STATE_START = 0006;
+    private static final int ADD_STATE_SEARCH = 0007;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,31 @@ public class AddAndViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_and_view);
 
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.map_fragment_container, MainMapFragment.newInstance(),null).commit();
+                .add(R.id.map_fragment_container, MainMapFragment.newInstance(ADD_STATE_START),null).commit();
 
+        findViewById(R.id.search_btn).setOnClickListener(this);
+        latInput = findViewById(R.id.lat_input);
+        longInput = findViewById(R.id.long_input);
+
+    }
+
+    // when search btn is clicked the edit text inputs are passed to the fragment as coordinates
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.search_btn){
+            String latStr = latInput.getText().toString();
+            String longStr = longInput.getText().toString();
+
+            if(!latStr.isEmpty() && !longStr.isEmpty()){
+                Double latD = Double.parseDouble(latStr);
+                Double longD = Double.parseDouble(longStr);
+
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.map_fragment_container, MainMapFragment.newInstance(ADD_STATE_SEARCH, latD, longD),null).commit();
+
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/example/knightjeffrey_myfishspots/fragments/MainMapFragment.java
+++ app/src/main/java/com/example/knightjeffrey_myfishspots/fragments/MainMapFragment.java
@@ -7,9 +7,7 @@ import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
 
 import com.example.knightjeffrey_myfishspots.R;
 import com.google.android.gms.maps.CameraUpdate;
@@ -24,15 +22,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainMapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener, View.OnClickListener {
+public class MainMapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapLongClickListener {
 
 
     private final double mStartLat = 36.898835;
     private final double mStartLong = -76.090170;
 
+    private Double searchLat;
+    private Double searchLong;
+
+    private Marker currentMarker;
+
     private static final int HOME_SCREEN_STATE = 0005;
-    private static final int ADD_NEW_STATE = 0006;
+    private static final int ADD_STATE_START = 0006;
+    private static final int ADD_STATE_SEARCH = 0007;
+
     private static final String STATE_KEY = "STATE_KEY";
+    private static final String LAT_KEY = "LAT_KEY";
+    private static final String LONG_KEY = "LONG_KEY";
     private int currentState;
 
 
@@ -43,15 +50,19 @@ public class MainMapFragment extends SupportMapFragment implements OnMapReadyCal
         Bundle args = new Bundle();
         MainMapFragment fragment = new MainMapFragment();
         args.putInt(STATE_KEY,state);
+
         fragment.setArguments(args);
         return fragment;
     }
 
-    public static MainMapFragment newInstance(int state, double _lat, double _long) {
+    public static MainMapFragment newInstance(int _state, double _lat, double _long) {
 
         Bundle args = new Bundle();
 
         MainMapFragment fragment = new MainMapFragment();
+        args.putInt(STATE_KEY,_state);
+        args.putDouble(LAT_KEY,_lat);
+        args.putDouble(LONG_KEY, _long);
         fragment.setArguments(args);
         return fragment;
     }
@@ -62,18 +73,21 @@ public class MainMapFragment extends SupportMapFragment implements OnMapReadyCal
         super.onActivityCreated(bundle);
         getMapAsync(this);
 
+
         if(getArguments() != null){
             switch (getArguments().getInt(STATE_KEY)){
                 case HOME_SCREEN_STATE:
                     currentState = HOME_SCREEN_STATE;
-                    zoomInCamera();
                     break;
-                case ADD_NEW_STATE:
-                    currentState = ADD_NEW_STATE;
+                case ADD_STATE_START:
+                    currentState = ADD_STATE_START;
+                    break;
+                case ADD_STATE_SEARCH:
+                    searchLat = getArguments().getDouble(LAT_KEY);
+                    searchLong = getArguments().getDouble(LONG_KEY);
+                    currentState = ADD_STATE_SEARCH;
                     break;
             }
-
-
 
         }
     }
@@ -86,16 +100,26 @@ public class MainMapFragment extends SupportMapFragment implements OnMapReadyCal
         mMap.setInfoWindowAdapter(this);
         mMap.setOnInfoWindowClickListener(this);
 
-        if(currentState == HOME_SCREEN_STATE){
-            addMapMarker(mStartLat, mStartLong);
-            zoomInCamera();
+        switch (currentState){
+            case HOME_SCREEN_STATE:
+                addMapMarker(mStartLat, mStartLong);
+                zoomInCamera(mStartLat,mStartLong);
+                break;
+            case ADD_STATE_START:
+                mMap.setOnMapLongClickListener(this);
+                break;
+            case ADD_STATE_SEARCH:
+                mMap.setOnMapLongClickListener(this);
+                addMapMarker(searchLat,searchLong);
+                zoomInCamera(searchLat,searchLong);
+                break;
         }
 
     }
 
     // TODO: change method to accept user input
     // add marker to the map ...
-    private void addMapMarker(double searchLat, double searchLong){
+    private void addMapMarker(Double searchLat, Double searchLong){
         if(mMap == null){
             return;
         }
@@ -107,17 +131,19 @@ public class MainMapFragment extends SupportMapFragment implements OnMapReadyCal
         LatLng officalLocation = new LatLng(searchLat,searchLong);
         options.position(officalLocation);
 
-        mMap.addMarker(options);
+        currentMarker = mMap.addMarker(options);
     }
+
+
 
     // TODO: change method to zoom on list view click
     // zoom in a specific map location..
-    private void zoomInCamera(){
+    private void zoomInCamera(Double searchLat, Double searchLong){
         if(mMap == null){
             return;
         }
 
-        LatLng officalLocation = new LatLng(mStartLat,mStartLong);
+        LatLng officalLocation = new LatLng(searchLat,searchLong);
         CameraUpdate cameraMovement = CameraUpdateFactory.newLatLngZoom(officalLocation, 16);
         mMap.animateCamera(cameraMovement);
     }
@@ -142,7 +168,14 @@ public class MainMapFragment extends SupportMapFragment implements OnMapReadyCal
     }
 
     @Override
-    public void onClick(View v) {
+    public void onMapLongClick(LatLng latLng) {
+        if(currentMarker != null){
+           currentMarker.remove();
+        }
+        addMapMarker(latLng.latitude,latLng.longitude);
+        zoomInCamera(latLng.latitude,latLng.longitude);
+
+
 
     }
 }
